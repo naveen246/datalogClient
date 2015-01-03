de576d92943fc7e9b3671b22baa4451e6f7318c9
Naveen <naveen246@gmail.com>
2015-01-03 18:46:47 +0530

    initial

---
+++ Client.java
@@ -0,0 +1,57 @@
+import java.net.*;
+import java.io.*;
+import java.nio.channels.*;
+import java.util.Date;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Date date = new Date();
+        System.out.println(date.toString());
+		String logType = args[0];
+		String secondParam = "";
+		String secondParamString = "";
+		URL url;
+		int valuesPerLine = 4;
+		if(logType.equals("n")) {
+			valuesPerLine = 16;
+		}
+		if(args.length > 1) {
+			secondParam = args[1];
+			if(logType.equals("f")) {
+				secondParamString = "&seconds=";
+			} else {
+				secondParamString = "&solenoids=";
+			}
+		}
+		url = new URL("http://169.254.101.102/?log=" + logType + secondParamString + secondParam + ";");
+		
+		BufferedReader in = null;
+		PrintWriter out = null;
+		
+		try {
+			in = new BufferedReader(new InputStreamReader(url.openStream()));
+			out = new PrintWriter("log.txt");
+			
+			int hiByte, lowByte, value, count = 0;
+			
+			while ( ((hiByte = in.read()) != -1) && ((lowByte = in.read()) != -1) )  {
+                value = ((hiByte << 8) | lowByte);
+                out.print(Integer.toString(value) + ',');
+                count++;
+                if(count % valuesPerLine == 0) {
+                    out.println();
+					out.flush();
+				}
+            }
+		} finally {
+			if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+            date = new Date();
+            System.out.println(date.toString());
+		}
+    }
+}
